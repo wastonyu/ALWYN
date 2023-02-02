31f8a81cd3e92cc3231536ade81bd0368bab0610
wastonyu <dayu-waston@outlook.com>
2023-02-02 10:53:31 +0800

    Update

--- disc_mm_type_list_h.sql
+++ disc_mm_type_list_h.sql
@@ -1,4 +1,7 @@
-CREATE TABLE [dbo].[disc_mm_type_list](
+CREATE TABLE [dbo].[disc_mm_type_list_h](
+	[h_add_time] [datetime] NOT NULL,
+	[h_add_user_id] [int] NOT NULL,
+	[h_add_station_id] [int] NULL,
 	[disc_mm_id] [int] NOT NULL,
 	[sub_id] [int] NOT NULL,
 	[type_a] [varchar](100) NULL,
@@ -7,6 +10,6 @@ CREATE TABLE [dbo].[disc_mm_type_list](
 	[type_b_id] [int] NULL,
 	[type_c] [varchar](100) NULL,
 	[type_c_id] [int] NULL,
-	[dispord] [int] NULL
- ) 
+	[dispord] [int] NULL,
+)
 ON [PRIMARY]
